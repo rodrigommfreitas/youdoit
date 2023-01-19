93761572cfa079fffe3a705495a15c3b18fc583c
Rodrigo Freitas <rodrigommfreitas@protonmail.com>
2023-01-19 23:17:14 +0000

    Add color palette

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        danube: {
+          50: "#f2f5fc",
+          100: "#e2e8f7",
+          200: "#ccd7f1",
+          300: "#a8bde8",
+          400: "#7f9bdb",
+          500: "#6882d3",
+          600: "#4c62c4",
+          700: "#4251b3",
+          800: "#3b4392",
+          900: "#343b74",
+        },
+      },
+    },
   },
   plugins: [],
 };
