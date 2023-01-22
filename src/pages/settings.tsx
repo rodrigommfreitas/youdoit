5112c18a61453d4974f86e018b34ca9c8283ed60
Rodrigo Freitas <rodrigommfreitas@protonmail.com>
2023-01-22 15:39:25 +0000

    Create settings page and secure server side

---
+++ src/pages/settings.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const settings: NextPage = () => {
+  return (
+    <main className="flex h-screen flex-col items-center justify-center gap-8 bg-danube-200 ">
+      <h1 className="text">settings</h1>
+    </main>
+  );
+};
+
+export default settings;
