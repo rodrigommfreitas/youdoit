9f8dc51ca3c39f7f3d06384d1051812f187fe2f1
Rodrigo Freitas <rodrigommfreitas@protonmail.com>
2023-01-25 17:25:47 +0000

    Merge task router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,5 @@
 import { createTRPCRouter } from "./trpc";
-import { exampleRouter } from "./routers/example";
+import { taskRouter } from "./routers/taskRouter";
 
 /**
  * This is the primary router for your server.
@@ -7,7 +7,7 @@ import { exampleRouter } from "./routers/example";
  * All routers added in /api/routers should be manually added here
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
+  task: taskRouter,
 });
 
 // export type definition of API
