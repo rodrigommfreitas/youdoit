423247e004eb627f93a85670d8815b2a5a5dbc88
Rodrigo Freitas <rodrigommfreitas@protonmail.com>
2023-01-25 17:23:24 +0000

    Create task router

---
+++ src/server/api/routers/taskRouter.ts
@@ -0,0 +1,105 @@
+import { z } from "zod";
+import { createTRPCRouter, protectedProcedure } from "../trpc";
+
+export const taskRouter = createTRPCRouter({
+  // Add a new task
+  addTask: protectedProcedure
+    .input(
+      z.object({
+        title: z
+          .string()
+          .min(1, { message: "Field can't be empty." })
+          .max(30, { message: "Maximum characters exceeded." }),
+        important: z.boolean(),
+        user: z.object({ connect: z.object({ id: z.string() }) }),
+      })
+    )
+    .mutation(async ({ input, ctx }) => {
+      try {
+        const item = await ctx.prisma.task.create({ data: input });
+        return item;
+      } catch (err) {
+        console.log(`It wasn't possible to add the task...\n ${err as string}`);
+      }
+    }),
+  // Get all tasks for a user
+  getTasks: protectedProcedure.query(async ({ ctx }) => {
+    try {
+      const tasks = await ctx.prisma.task.findMany({
+        where: { user: { id: ctx.session.user.id } },
+      });
+      return tasks;
+    } catch (err) {
+      console.log(
+        `It wasn't possible to find your tasks...\n ${err as string}`
+      );
+    }
+  }),
+  // Toggle a task's important status
+  toggleImportant: protectedProcedure
+    .input(
+      z.object({
+        id: z.string(),
+      })
+    )
+    .mutation(async ({ input, ctx }) => {
+      try {
+        const task = await ctx.prisma.task.findUnique({
+          where: { id: input.id },
+        });
+        const newTask = await ctx.prisma.task.update({
+          where: { id: input.id },
+          data: { important: !task?.important },
+        });
+        return newTask;
+      } catch (err) {
+        console.log(
+          `It wasn't possible to toggle the task's important status...\n ${
+            err as string
+          }`
+        );
+      }
+    }),
+  // Toggle a task's completed status
+  toggleCompleted: protectedProcedure
+    .input(
+      z.object({
+        id: z.string(),
+      })
+    )
+    .mutation(async ({ input, ctx }) => {
+      try {
+        const task = await ctx.prisma.task.findUnique({
+          where: { id: input.id },
+        });
+        const item = await ctx.prisma.task.update({
+          where: { id: input.id },
+          data: { completed: !task?.completed },
+        });
+        return item;
+      } catch (err) {
+        console.log(
+          `It wasn't possible to toggle the task's completed status...\n ${
+            err as string
+          }`
+        );
+      }
+    }),
+  // Delete a task
+  deleteTask: protectedProcedure
+    .input(
+      z.object({
+        id: z.string(),
+      })
+    )
+    .mutation(async ({ input, ctx }) => {
+      try {
+        const item = await ctx.prisma.task.delete({ where: { id: input.id } });
+        return item;
+      } catch (err) {
+        console.log(
+          `It wasn't possible to delete the task...\n ${err as string}`
+        );
+      }
+    }),
+});
